754a4b8dac0636079d10bfe800261163e33d8d02
Shel Orock <sdo243@nyu.edu>
2018-04-03 07:39:53 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -3,9 +3,9 @@ function forLoop(array) {
   for(let i = 1; i <= 25; i++) {
     array = [];
       if(i === 1) {
-      array.push('I am ${i} strange loop.');
+      array.push('I am ' + ${i} + ' strange loop.');
       } else {
-      array.push('I am ${i} strange loops.');
+      array.push('I am ' + ${i} + 'strange loops.');
     }
   return array;
   }
