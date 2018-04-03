879b49d66895428a1b5a7abc6f742283b94194ec
Shel Orock <sdo243@nyu.edu>
2018-04-03 07:51:39 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,8 @@
 
 function forLoop(array) {
-  var myArray = array;
   for(let i = 0; i < 25; i++) {
       if(i === 1) {
-      myArray.push(`I am 1 strange loop.`);
+      array.push(`I am 1 strange loop.`);
       } else {
       myArray.push(`I am ${i} strange loops.`);
     }
