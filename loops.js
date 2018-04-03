9c69a68c708aa3d8e6d7e89acf0a6c6fb8954f45
Shel Orock <sdo243@nyu.edu>
2018-04-03 08:01:19 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -18,5 +18,13 @@ function whileLoop(n) {
 }
 
 function doWhileLoop(array) {
-  
+  function maybeTrue() {
+  return Math.random() >= 0.5
+  }
+  do {
+    --array.length
 }
+while(array.length > 0 && maybeTrue()) ;
+}
+
+return array
